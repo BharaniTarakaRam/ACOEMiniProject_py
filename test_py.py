8b1ba2fb1b9a387b20f066acd3f0e505ed12e18c
BharaniTarakaRam <105097147+BharaniTarakaRam@users.noreply.github.com>
2022-05-07 11:19:30 +0530

    upper care or lower case

--- test_py.py
+++ test_py.py
@@ -1,1 +1,12 @@
-print("welcome to git")
+word = "SAI SRI BHARANI TARAKA RAM KUMAR"
+n = len(word)
+word1 = word.upper()
+word2 = word.lower()
+converted_word = ""
+for i in range(n):
+ if i % 2 == 0:
+   converted_word += word2[i]
+ else:
+   converted_word += word1[i]
+print("using converted-method::",converted_word)
+print("With out using converted-method::",word)
